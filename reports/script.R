d14ff8623e8ffea872e84d71085a33323db8f0ba
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2017-12-14 14:27:45 +0000

    modifications

--- reports/script.R
+++ reports/script.R
@@ -15,103 +15,93 @@ library(progress)
 library(rsconnect)
 library(shinyloadtest)
 
-# functions ---------------------------------------------------------------
-
-simulate <- function(p) {
-  outdir <- paste0("output_", p$id, "_", p$app, "_", p$concurrency, "usr_", p$srv_conf, "_", p$duration)
-  # if (!dir.exists(outdir)) dir.create(outdir)
-  outpath <- "~/shiny-concurrency/shiny-server-pro/4_json"
-  command <- paste0("cd ", outpath, "; ",
-                    "rm -f ", outdir, "/*; ",
-                    "mkdir -p ", outdir, "; ",
-                    "~/shiny-concurrency/proxyrec playback ",
-                    "--target 'http://ec2-52-201-221-45.compute-1.amazonaws.com:3838/", p$app, "/' ",
-                    "--outdir ", outdir,
-                    " --concurrency ", p$concurrency, "  --duration '", p$duration, "' ",
-                    "../../tests/4_json/app-recording.txt &")
-  message("running: ", command)
-  out <-
-    system(command, inter = FALSE)
-  message("polling..")
-  df <-
-    shinyloadtest::poll(
-      servers = c('ec2-52-201-221-45.compute-1.amazonaws.com:3838'),
-      appName = 'shinyTest',
-      duration_sec = p$time_monitor,
-      platform = 'ssp'
-    )
-  list(poll       = df,
-       outdir     = file.path(outpath, outdir),
-       parameters = parameters,
-       command    = command)
-}
-
-report_gen <- function(sim) {
-  report_path = '~/shiny-concurrency/reports/shiny-server-pro'
-  report_template_name = "report.Rmd"
-  report_tmp = file.path(report_path, "report.html")
-  #createLoadTestReport(dir = report_path, name = report_template_name)
-  report_template <- file.path(report_path, report_template_name)
-  directory   = sim$outdir #'~/shiny-concurrency/shiny-server-pro/4_json/output_10_mysql_001usr_4thr_100-090-40_2min_net/'
-  output = strsplit(directory, split = "/")[[1]][5]
-  report_file = file.path( report_path, paste0('report_', output, '.html') )
-
-  rmarkdown::render(report_template,
-                    params = list(directory = directory))
-  file.rename(report_tmp, report_file)
-}
-
 # main --------------------------------------------------------------------
 
-sim_param_1 <- list(id = "30",
-                    app = "shinyTest",
-                    concurrency = "1",
-                    srv_conf = "100-090-40_net",
-                    duration = "180sec",
-                    time_monitor = 30)
-sim_param_2 <- list(id = "31",
-                    app = "shinyTest",
-                    concurrency = "200",
-                    srv_conf = "100-090-40_net",
-                    duration = "180sec",
-                    time_monitor = 30)
+shiny_server <- "ec2-52-91-131-141.compute-1.amazonaws.com"
 
-sim_1_out <-
-  sim_param_1 %>%
-  simulate
-report_gen(sim_1_out)
+sim_param_1 <- Simulation(id = "60",
+                          app = "shinyTest",
+                          concurrency = "1",
+                          srv_conf = "100-090-40_net",
+                          duration = "60sec",
+                          time_monitor = 30,
+                          shiny_server = shiny_server,
+                          shiny_port   = "3838")
+sim_param_2 <- Simulation(id = "61",
+                          app = "shinyTest",
+                          concurrency = "200",
+                          srv_conf = "100-090-40_net",
+                          duration = "180sec",
+                          time_monitor = 30,
+                          shiny_server = shiny_server,
+                          shiny_port   = "3838")
+app_url(sim_param_1)
 
-sim_param_2 %>%
-  simulate %>%
-  report_gen
-
-
-
+cat(simulate_cmd(sim_param_1))
+sim_1_out <- simulate(sim_param_1)
+sim_1_rep <- report_gen(sim_1_out)
+(sim_1_rep$file)
 unlink('~/shiny-concurrency/shiny-server-pro/4_json/output_canc//profile_173_2.txt')
 
 # reports -----------------------------------------------------------------
 
-l = list(poll       = NA,
-     outdir     = "~/shiny-concurrency/shiny-server-pro/4_json/output_10_mysql_001usr_4thr_100-090-40_2min_net/",
-     parameters = NA,
-     command    = NA)
+l = SimOut(poll       = NA,
+           outdir     = "~/shiny-concurrency/shiny-server-pro/4_json/output_10_mysql_001usr_4thr_100-090-40_2min_net/",
+           parameters = NA,
+           command    = NA)
+str(l)
 log <- createLog(l$outdir)
 
 report_gen(l)
 
-list(poll       = NA,
-     outdir     = "~/shiny-concurrency/shiny-server-pro/4_json/output_11_mysql_200usr_4thr_100-090-40_2min_net/",
-     parameters = NA,
-     command    = NA) %>%
+SimOut(poll       = NA,
+       outdir     = "~/shiny-concurrency/shiny-server-pro/4_json/output_11_mysql_200usr_4thr_100-090-40_2min_net/",
+       parameters = NA,
+       command    = NA) %>%
   report_gen
-list(poll       = NA,
-     outdir     = "~/shiny-concurrency/shiny-server-pro/4_json/output_54_mysql_200usr_4thr_100-090-40_6min_vb/",
-     parameters = NA,
-     command    = NA) %>%
+SimOut(poll       = NA,
+       outdir     = "~/shiny-concurrency/shiny-server-pro/4_json/output_54_mysql_200usr_4thr_100-090-40_6min_vb/",
+       parameters = NA,
+       command    = NA) %>%
   report_gen
-list(poll       = NA,
-     outdir     = "~/shiny-concurrency/shiny-server-pro/4_json/output_52_mysql_001usr_4thr_100-090-40_2min_vb/",
-     parameters = NA,
-     command    = NA) %>%
+SimOut(poll       = NA,
+       outdir     = "~/shiny-concurrency/shiny-server-pro/4_json/output_52_mysql_001usr_4thr_100-090-40_2min_vb/",
+       parameters = NA,
+       command    = NA) %>%
   report_gen
 
+
+# shiny-proxy -------------------------------------------------------------
+
+shiny_proxy <- "ec2-35-153-232-44.compute-1.amazonaws.com"
+frame <- '    <iframe id="shinyframe" width="100%" src="/elated_carson/"></iframe>'
+
+sim_param_3 <- SimulationProxy(id = "70",
+                               app = "test_app",
+                               concurrency = "1",
+                               srv_conf = "100-090-40_net",
+                               duration = "60sec",
+                               time_monitor = 30,
+                               shiny_server = shiny_proxy,
+                               shiny_port   = "8080")
+
+
+url <- paste0(app_url.SimulationProxy(sim_param_3), "/")
+url
+require(RCurl)
+
+r = dynCurlReader()
+str(r)
+r$reset()                               # reset
+curlPerform(
+  #postfields = json_1,
+            url = url,                  # or uri
+            verbose = TRUE,
+            # userpwd = usr_pwd,
+            # httpauth = 1L,
+            # post = 1L,
+            writefunction = r$update)   # append the result
+html <- shinyProxyPage
+html <- r$value()
+strsplit(html, split = "\n")[[1]][82]
+substr()
